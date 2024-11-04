7e4967c536bc797b398ccc84e4e293268cd496ec
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2024-11-04 15:55:17 +0100

    fixed

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -13,9 +13,9 @@ class Docker implements Serializable {
         script.echo "building the docker image..."
         script.withCredentials([script.usernamePassword(credentialsId: 'docker_hub_repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "docker build -t $imageName ."
-            script.sh '''
+            script.sh """
             echo $script.PASS | docker login -u $script.USER --password-stdin
-            '''
+            """
             script.sh "docker push $imageName"
         }
     }
