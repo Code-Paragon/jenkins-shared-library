2a61e27153add45da73f27330a03d3d794d1dae8
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2024-11-04 14:40:09 +0100

    fixed naem error

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,10 +3,10 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker_hub_repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t retr0009/demo-app:jma-2.0 .'
         sh '''
         echo $PASS | docker login -u $USER --password-stdin
         '''
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push retr0009/demo-app:jma-2.0'
     }
 }
