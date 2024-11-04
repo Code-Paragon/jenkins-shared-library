d6faf8463871dff9c106690e67a567f97dcdb458
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2024-11-04 15:04:00 +0100

    implemented parameters and global variables

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker_hub_repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t retr0009/demo-app:jma-2.0 .'
+        sh "docker build -t $imageName ."
         sh '''
         echo $PASS | docker login -u $USER --password-stdin
         '''
-        sh 'docker push retr0009/demo-app:jma-2.0'
+        sh "docker push $imageName"
     }
 }
